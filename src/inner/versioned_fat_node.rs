01de4e5eddfaf9fb312a6c70321decc189d42786
Taras Pashkin <0x2493@gmail.com>
2014-12-18 12:17:27 +0600

    option -> x, treemap -> hashmap

--- src/inner/versioned_fat_node.rs
+++ src/inner/versioned_fat_node.rs
@@ -3,20 +3,21 @@
  *  supports undo-redo.
  */
 
-use std::cell::RefCell;
-use std::collections::tree_map::TreeMap;
+use std::cell::RefCell as RCell;
+use std::collections::TreeMap as TMap;
+use std::collections::HashMap as HMap;
 use std::rc::Rc;
 use std::vec::Vec;
 
 
 
 pub struct VersionTree {
-    parent: TreeMap<i64, i64>,
+    parent: TMap<i64, i64>,
 }
 
 impl VersionTree {
     pub fn new(initial_revision: i64) -> VersionTree {
-        let mut p = TreeMap::new();
+        let mut p = TMap::new();
         p.insert(initial_revision, -1);
         VersionTree{parent: p}
     }
@@ -93,51 +94,37 @@ fn version_tree_test() {
 
 
 
-pub struct VersionedFatNode<T> {
-    values: TreeMap<i64, Option<Rc<T>>>, // TODO
-                                         // lower_bound() no longer required
-                                         // can be replaced by HashMap
-    revisions: Rc<RefCell<VersionTree>>
+pub struct VersionedFatNode<T: Clone> {
+    values: HMap<i64, T>,
+    revisions: Rc<RCell<VersionTree>>
 }
 
-impl<T> VersionedFatNode<T> {
-    pub fn new(rev: Rc<RefCell<VersionTree>>) -> VersionedFatNode<T> {
-        let mut map = TreeMap::new();
-        map.insert(0, None);
+impl<T: Clone> VersionedFatNode<T> {
+    pub fn new(rev: Rc<RCell<VersionTree>>) -> VersionedFatNode<T> {
+        let map = HMap::new();
         VersionedFatNode{values: map, revisions: rev.clone()}
     }
 
-    pub fn value<'a>(&'a self, revision: i64) -> Option<Rc<T>> {
+    pub fn value(&self, revision: i64) -> Option<T> {
         for cur_rev in self.revisions.borrow().parent_branch(revision).iter() {
-            match self.values.find(cur_rev) {
-                None => continue,
-                Some(ref value) =>
-                    match *value {
-                        &None =>
-                            return None,
-                        &Some(ref rct) =>
-                            return Some(rct.clone())
-                    }
+            match self.values.get(cur_rev) {
+                None            => continue,
+                Some(&ref value) => return Some(value.clone())
             }
         }
         None
     }
 
-    pub fn add_value(&mut self, new_revision: i64, real_value: Option<T>, old_revision: i64) {
+    pub fn add_value(&mut self, new_revision: i64, value: T, old_revision: i64) {
         self.revisions.borrow_mut().insert(new_revision, old_revision);
-        match real_value {
-            None =>
-                self.values.insert(new_revision, None),
-            Some(real_value) =>
-                self.values.insert(new_revision, Some(Rc::new(real_value))),
-        };
+        self.values.insert(new_revision, value.clone());
     }
 }
 
 #[test]
 fn versioned_fatnode_test() {
-    let mut vs = VersionTree::new(1);
-    let mut vf = VersionedFatNode::<&str>::new(Rc::new(RefCell::new(vs)));
+    let vs = VersionTree::new(1);
+    let mut vf = VersionedFatNode::<&str>::new(Rc::new(RCell::new(vs)));
 
     /*
      * Build the same tree as above:
@@ -150,23 +137,23 @@ fn versioned_fatnode_test() {
      *       \
      *        3--5--8
      */
-    vf.add_value(2, Some("two"), 1);
-    vf.add_value(3, Some("three"), 2);
-    vf.add_value(4, Some("four"), 2);
-    vf.add_value(5, Some("five"), 3);
-    vf.add_value(6, Some("six"), 4);
-    vf.add_value(7, Some("seven"), 4);
-    vf.add_value(8, Some("eight"), 5);
+    vf.add_value(2, "two", 1);
+    vf.add_value(3, "three", 2);
+    vf.add_value(4, "four", 2);
+    vf.add_value(5, "five", 3);
+    vf.add_value(6, "six", 4);
+    vf.add_value(7, "seven", 4);
+    vf.add_value(8, "eight", 5);
 
-    assert_eq!(*vf.value(2).unwrap(), "two");
-    assert_eq!(*vf.value(5).unwrap(), "five");
-    assert_eq!(*vf.value(7).unwrap(), "seven");
+    assert_eq!(vf.value(2).unwrap(), "two");
+    assert_eq!(vf.value(5).unwrap(), "five");
+    assert_eq!(vf.value(7).unwrap(), "seven");
     assert_eq!(vf.value(1), None);
 }
 
 #[test]
 fn three_fatnodes_test() {
-    let mut vs = Rc::new(RefCell::new(VersionTree::new(1)));
+    let vs = Rc::new(RCell::new(VersionTree::new(1)));
     let mut vfa = VersionedFatNode::<&str>::new(vs.clone());
     let mut vfb = VersionedFatNode::<&str>::new(vs.clone());
     let mut vfc = VersionedFatNode::<&str>::new(vs.clone());
@@ -182,23 +169,23 @@ fn three_fatnodes_test() {
      *       \
      *        3a--5b--8a
      */
-    vfa.add_value(2, Some("two"), 1);
-    vfa.add_value(3, Some("three"), 2);
-    vfc.add_value(4, Some("four"), 2);
-    vfb.add_value(5, Some("five"), 3);
-    vfb.add_value(6, Some("six"), 4);
-    vfc.add_value(7, Some("seven"), 4);
-    vfa.add_value(8, Some("eight"), 5);
+    vfa.add_value(2, "two", 1);
+    vfa.add_value(3, "three", 2);
+    vfc.add_value(4, "four", 2);
+    vfb.add_value(5, "five", 3);
+    vfb.add_value(6, "six", 4);
+    vfc.add_value(7, "seven", 4);
+    vfa.add_value(8, "eight", 5);
 
-    assert_eq!(*vfa.value(8).unwrap(), "eight");
-    assert_eq!(*vfb.value(8).unwrap(), "five");
-    assert_eq!( vfc.value(8), None);
+    assert_eq!(vfa.value(8).unwrap(), "eight");
+    assert_eq!(vfb.value(8).unwrap(), "five");
+    assert_eq!(vfc.value(8), None);
 
-    assert_eq!(*vfa.value(5).unwrap(), "three");
-    assert_eq!(*vfb.value(5).unwrap(), "five");
-    assert_eq!( vfc.value(5), None);
+    assert_eq!(vfa.value(5).unwrap(), "three");
+    assert_eq!(vfb.value(5).unwrap(), "five");
+    assert_eq!(vfc.value(5), None);
 
-    assert_eq!(*vfa.value(6).unwrap(), "two");
-    assert_eq!(*vfb.value(6).unwrap(), "six");
-    assert_eq!(*vfc.value(6).unwrap(), "four");
+    assert_eq!(vfa.value(6).unwrap(), "two");
+    assert_eq!(vfb.value(6).unwrap(), "six");
+    assert_eq!(vfc.value(6).unwrap(), "four");
 }
