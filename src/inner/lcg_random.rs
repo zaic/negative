790854e977b5415ae1e869be47b9dd50a49da981
Vadim Zaytsev <zaic101@gmail.com>
2014-12-23 11:18:51 +0600

    commit ideas about STM

--- src/inner/lcg_random.rs
+++ src/inner/lcg_random.rs
@@ -37,6 +37,8 @@ fn cool_lcg() {
     assert!(one != two);
 }
 
+
+
 pub struct DebugLCG {
     x: u64,
 }
@@ -59,5 +61,66 @@ fn debug_lcg() {
     let mut rnd : DebugLCG = LCG::new();
     for i in range(1i, 10) {
         assert!(rnd.next() == i);
+    }
+}
+
+
+
+pub struct TransactionLCG {
+    x: u64,
+
+    stm_clock: int,
+    stm_uid: int,
+    /* rwlock */
+}
+
+impl Transactioned for TransactionLCG {
+    fn check(&self) -> bool {
+        // compare sefl.stm_clock and clock from read_log
+    }
+
+    fn commit(&mut self) {
+        // get self.x from write_log and store it
+    }
+
+    fn unroll(&self) {
+        // hmm... do nothing
+    }
+}
+
+impl LCG for TransactionLCG {
+    fn new() -> TransactionLCG {
+        TransactionLCG {
+            inner_lcg: CoolLCG::new(),
+            stm_clock: -1,
+            stm_uid: 0, // TODO get global uid
+        }
+    }
+
+    fn next() -> int {
+        // 0. lock rwlock
+        //
+        // 1. verify global clock
+        // 1a. fail transaction if clock was incremented (?)
+        // 1b. insert clock value to read_log if it was'n present before
+        //
+        // 2. try to find self in write_log
+        // 2a. use self.x from write_log
+        // 2b. use self.x from self
+        //
+        // 3. generate next value
+        //
+        // 4. store new x to write_log
+        //
+        // 5. unlock rwlock
+        //
+        //
+        //
+        // steps 0, 1[ ab] and 5 should be move out
+        //
+        // TLS is a thread-local map: stm_uid -> Something
+        //   read_log is a map: stm_uid -> ClockType
+        //   write_log is a map: stm_uid -> lamda (?)
+        //   one more map: stm_uid -> &Transactioned
     }
 }
