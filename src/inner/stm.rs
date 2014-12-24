a1fc80a2752356cbc9e901147bf413229d5cf10a
Vadim Zaytsev <zaic101@gmail.com>
2014-12-24 15:54:25 +0600

    Thread local storage using example.

--- src/inner/stm.rs
+++ src/inner/stm.rs
@@ -2,9 +2,26 @@
  *  Initial STM implementation
  */
 
-use std::collections::BTreeMap as TreeMap;
-use std::rc::Rc;
-use inner::persistent::Revision;
+pub use std::sync::RWLock;
+pub use std::collections::HashMap;
+
+pub struct TLS {
+    pub initial_time: int,
+    pub failed: bool,
+    pub log_read: HashMap<int, int>,
+    pub log_write: HashMap<int, u64>, // TODO one more fix required...
+}
+
+impl TLS {
+    pub fn new() -> TLS {
+        TLS {
+            initial_time: 0, // TODO use global clock
+            failed: false,
+            log_read: HashMap::new(),
+            log_write: HashMap::new(),
+        }
+    }
+}
 
 pub trait Transactioned {
     // these function like a gopher
@@ -12,6 +29,6 @@ pub trait Transactioned {
     // fn modify();
 
     fn check(&self) -> bool;
-    fn commit(&mut self, lambda); // быть может, вообще выпилить эту и следующую функцию
+    fn commit(&mut self, |&Transactioned| -> int); // быть может, вообще выпилить эту и следующую функцию
     fn unroll(&self);
 }
